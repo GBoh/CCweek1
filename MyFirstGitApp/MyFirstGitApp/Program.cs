d4b766f9db302aab245bf8b1e658a806e5336bda
GBoh <greg@bohanon3d.com>
2015-08-17 17:53:28 -0500

    Updated program to now ask for a date and convert it to a long date format and display

--- MyFirstGitApp/MyFirstGitApp/Program.cs
+++ MyFirstGitApp/MyFirstGitApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,17 @@ namespace MyFirstGitApp
     {
         static void Main(string[] args)
         {
+            //informs the user of what to do
+            Console.WriteLine("Enter you date");
 
-            Console.Write(DateTime.Now);
+
+            var dateString = Console.ReadLine();
+            Console.WriteLine(dateString); //not needed just to double check dateString is being read from ReadLine
+            Console.WriteLine(DateTime.Parse(dateString).ToString("D"));
+
+            //to hold the code at the end to double check
             Console.ReadLine();
+
 
         }
     }
