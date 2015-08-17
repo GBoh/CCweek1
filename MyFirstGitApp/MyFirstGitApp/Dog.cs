0aa45ab446e7414c8e8be5b8ce061bbbf72c94c8
GBoh <greg@bohanon3d.com>
2015-08-17 15:48:45 -0500

    Added simple program to check date time

---
+++ MyFirstGitApp/MyFirstGitApp/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitApp
+{
+    class Dog
+    {
+
+        // lbs
+        public int Weight = 100;
+
+        public string Color = @"brown";
+
+        public void Bark()
+        {
+            Console.WriteLine(@"woof");
+        }
+    }
+}
--- MyFirstGitApp/MyFirstGitApp/Program.cs
+++ MyFirstGitApp/MyFirstGitApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace MyFirstGitApp
     {
         static void Main(string[] args)
         {
+
+            Console.Write(DateTime.Now);
+            Console.ReadLine();
         }
     }
 }
