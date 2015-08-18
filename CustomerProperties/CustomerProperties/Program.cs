f3b57d2c47b5abd30c09cd32c760249aba17381a
GBoh <greg@bohanon3d.com>
2015-08-18 14:59:37 -0500

    Sets customer age and checks to make sure age is correct

--- CustomerProperties/CustomerProperties/Program.cs
+++ CustomerProperties/CustomerProperties/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace CustomerProperties
     {
         static void Main(string[] args)
         {
+            var customer1 = new customer
+            {
+                FirstName = "Sally",
+                LastName = "Williams",
+                Age = 23,
+                Birthday = DateTime.Parse("01/01/2028")
+            };
+
+            var customer2 = new customer
+            {
+                FirstName = "Mike",
+                LastName = "Harrison",
+                Age = 10, //default 10, test with negative age
+                Birthday = DateTime.Parse("01/01/2019")
+            };
+
+            //var customer3 = new customer
+            //{
+            //    firstName = Console.ReadLine(),
+            //    lastName = Console.ReadLine(),
+            //    age = int.Parse(Console.ReadLine()),
+            //    birthday = int.Parse(Console.ReadLine())
+            //};
+
+            Console.WriteLine("Customer {0} {1} is {2} years old and was born in year {3}", customer2.FirstName, customer2.LastName, customer2.Age, customer2.Birthday);
+
+            Debug.Assert(customer1.Age == 23, "Sally is 23.");
+            //Debug.Assert(customer2.age == 10, "Mike is 10.");
+            //Console.WriteLine("Thank you for joining {0} {1}", customer3.firstName, customer3.lastName);
+            Console.ReadLine();
         }
     }
 }
---
+++ CustomerProperties/CustomerProperties/customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerProperties
+{
+    class customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Debug.Assert(value == 0, "Age cannot be younger than 0.");
+                }
+                _age = value;
+            }
+        }
+    }
+}
