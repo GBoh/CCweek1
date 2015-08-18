ab7f33fa575973d6c32ffdd558329c3a3e40019d
GBoh <greg@bohanon3d.com>
2015-08-18 13:41:15 -0500

    Added contrarian

---
+++ contrarian/contrarian/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contrarian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //get user message
+            //find the world "like"
+            //replace the word like with "don't like"
+            //ExC. Generalize to include other worlds.
+            var userMessage = Console.ReadLine();
+            //var lower = userMessage.ToLower();
+            //Console.WriteLine(lower);
+
+
+            Console.ReadLine();
+        }
+    }
+}
