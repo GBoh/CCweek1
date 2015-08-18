43d2d6cb14f8bf687b0508c22fb012c572bace88
GBoh <greg@bohanon3d.com>
2015-08-18 13:24:30 -0500

    Added a new DivisibleBySeven program and got the audio to work in homework

---
+++ DivisibleBySeven/DivisibleBySeven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleBySeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 0; i<100; i++)
+            {
+                var number = i % 7;
+                if (number == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- PlayMovieQuotes/PlayMovieQuotes/Program.cs
+++ PlayMovieQuotes/PlayMovieQuotes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace PlayMovieQuotes
 {
@@ -10,6 +11,42 @@ namespace PlayMovieQuotes
     {
         static void Main(string[] args)
         {
+
+            //detects if there is a connection and displays a message accordingly
+            var message = (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable()) ? "Enjoy the sound clip!" : "Sorry no connection";
+            Console.WriteLine(message);
+
+            //plays a sound clip if there is a network connection
+            if (message == "Enjoy the sound clip!") {
+
+                //gets a random number
+                var player =  new SoundPlayer();
+                Random rnd = new Random();
+                switch (rnd.Next(0, 4))
+                {
+                    case 1:
+                        Console.WriteLine("You feeling lucky punk?");
+                        player = new SoundPlayer(@"http://www.wavsource.com/snds_2015-08-16_6897529750891327/movie_stars/eastwood/punk.wav");
+                        player.PlaySync();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Surely you're not serious");
+                        player = new SoundPlayer(@"http://www.wavsource.com/snds_2015-08-16_6897529750891327/movies/airplane/surely_a.wav");
+                        player.PlaySync();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Holy hole");
+                        player = new SoundPlayer(@"http://www.wavsource.com/snds_2015-08-16_6897529750891327/tv/batman/holy_hole.wav");
+                        player.PlaySync();
+                        break;
+
+                }
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
