a2ea6a5f3bf685dbfe0c4ef500f024150afc389c
GBoh <greg@bohanon3d.com>
2015-08-18 16:26:55 -0500

    Add any amount of numbers together

---
+++ AddNumbers/AddNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = addNumbers(2, 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int addNumbers(params int[] number)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += number[i];
+            }
+
+            return sum;
+        }
+    }
+}
