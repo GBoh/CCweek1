bcfffa8d53343d580450b1149456f9e61c79b99b
GBoh <greg@bohanon3d.com>
2015-08-19 16:16:34 -0500

    RockPaperScisssors Program base

--- day2/RockPaperScisssors/RockPaperScisssors/Program.cs
+++ day2/RockPaperScisssors/RockPaperScisssors/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace RockPaperScisssors
 {
+    public interface IPlayer
+    {
+        int NumberOfWins { get; set; }
+        string Act();
+    }
+
+
+
+
+
+    class PlayerRock : IPlayer
+    {
+        public int NumberOfWins
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        public string Act()
+        {
+            return "";
+        }
+    }
+
+
+
+
+
+
+    class Game
+    {
+        public IPlayer Fight(IPlayer player1, IPlayer player2)
+        {
+
+            return IPlayer;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                rnd.Next(3);
+                //Console.WriteLine(rnd.Next(3));
+            }
+
+            Console.ReadLine();
         }
     }
 }
