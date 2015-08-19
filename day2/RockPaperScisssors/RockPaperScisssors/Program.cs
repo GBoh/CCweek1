0d0124b8d333a98a1119ec7df8cec11b7480b963
GBoh <greg@bohanon3d.com>
2015-08-19 18:08:19 -0500

    RockPaperScisssors is working wooo

--- day2/RockPaperScisssors/RockPaperScisssors/Program.cs
+++ day2/RockPaperScisssors/RockPaperScisssors/Program.cs
@@ -16,20 +16,12 @@ namespace RockPaperScisssors
     {
         public int NumberOfWins
         {
-            get
-            {
-                return 0;
-            }
-
-            set
-            {
-                //throw new NotImplementedException();
-            }
+            get; set;
         }
 
         public string Act()
         {
-            return "";
+            return "rock";
         }
     }
 
@@ -37,42 +29,49 @@ namespace RockPaperScisssors
     {
         public int NumberOfWins
         {
-            get
-            {
-                return 0;
-            }
-
-            set
-            {
-                //throw new NotImplementedException();
-            }
+            get; set;
         }
 
         public string Act()
         {
-            return "";
+            return "scissors";
         }
     }
 
+    class PlayerPaper : IPlayer
+    {
+        public int NumberOfWins
+        {
+            get; set;
+        }
 
-
-
-
+        public string Act()
+        {
+            return "paper";
+        }
+    }
 
     class Game
     {
         public static IPlayer Fight(IPlayer player1, IPlayer player2)
         {
-            if (player1 == PlayerRock && player2 == PlayerSciccors)
+            if (player1.Act() == "rock" && player2.Act() == "scissors")
             {
-                player1.Act();
                 player1.NumberOfWins++;
                 return player1;
             }
-            else
+            if (player1.Act() == "rock" && player2.Act() == "paper")
             {
+                player2.NumberOfWins++;
                 return player2;
             }
+            if (player1.Act() == "scissors" && player2.Act() == "paper")
+            {
+                player1.NumberOfWins++;
+                return player1;
+            }
+
+            return null;
         }
     }
 
@@ -81,21 +80,56 @@ namespace RockPaperScisssors
     {
         static void Main(string[] args)
         {
+            var rock = new PlayerRock();
+            var scissors = new PlayerSciccors();
+            var paper = new PlayerPaper();
+
+
 
             Random rnd = new Random();
             for (int i = 0; i < 100; i++)
             {
-                var game = rnd.Next(1);
+                var game = rnd.Next(3);
                 switch (game)
                 {
                     case 0:
-                        Game.Fight(PlayerRock, PlayerSciccors);
+                        Game.Fight(rock, scissors);
+                        break;
+                    case 1:
+                        Game.Fight(rock, paper);
+                        break;
+                    case 2:
+                        Game.Fight(scissors, paper);
                         break;
                 }
                 //Console.WriteLine(rnd.Next(3));
             }
+            Console.WriteLine("Rock won {0} times", rock.NumberOfWins);
+            Console.WriteLine("Paper won {0} times", paper.NumberOfWins);
+            Console.WriteLine("Scissor won {0} times", scissors.NumberOfWins);
+
+            var winner = MostWins(rock, paper, scissors);
+
+            Console.WriteLine("the winner is " + winner);
+
 
             Console.ReadLine();
+        }
+
+        public static string MostWins(params IPlayer[] inputs)
+        {
+            var highestScore = 0;
+            string winner = null;
+            for (int i = 0; i < 3; i++)
+            {
+                if (inputs[i].NumberOfWins > highestScore)
+                {
+                    highestScore = inputs[i].NumberOfWins;
+                    winner = inputs[i].Act();
+                }
+            }
+
+            return winner;
         }
     }
 }
--- day3/RandomNewsWithArrayList/RandomNewsWithArrayList/Program.cs
+++ day3/RandomNewsWithArrayList/RandomNewsWithArrayList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace RandomNewsWithArrayList
     {
         static void Main(string[] args)
         {
+            var newsTick = new ArrayList();
+            newsTick.Add("Life gave Cave Johnson lemons");
+            newsTick.Add("Cave Johnson had his engineers make lemon grenades");
+            newsTick.Add("Life was blown up");
+
+            Random rnd = new Random();
+            Console.WriteLine(newsTick[rnd.Next(3)]);
+            Console.ReadLine();
         }
     }
 }
