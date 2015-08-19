d127bd33a1fc1ce100583a12e6c89d5233873586
GBoh <greg@bohanon3d.com>
2015-08-19 10:13:37 -0500

    fixed constructor project

--- day2/ProductWithConstructor/ProductWithConstructor/Product.cs
+++ day2/ProductWithConstructor/ProductWithConstructor/Product.cs
@@ -13,17 +13,12 @@ namespace ProductWithConstructor
         public int Stock { get; set; }
         public string Description { get; set; }
 
-        public Product(string name, decimal? price, int? inStock,  string description)
+        public Product(string name, decimal price, int stock, string description = "NO DESCRIPTION")
         {
-            if(name != null && price.HasValue && inStock.HasValue)
-            {
-                Console.WriteLine("you have met the 3 minimum requirements");
-                Console.WriteLine("The item {0} costs {1} and there are {2} left:\n{3}", name, price, inStock, description);
-            }
-            else
-            {
-                Console.WriteLine("You are missing a requirment");
-            }
+            Name = name;
+            Price = price;
+            Stock = stock;
+            Description = description;
         }
     }
 }
--- day2/ProductWithConstructor/ProductWithConstructor/Program.cs
+++ day2/ProductWithConstructor/ProductWithConstructor/Program.cs
@@ -11,14 +11,21 @@ namespace ProductWithConstructor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input name of poduct, price, and stock ammount. Description Optional");
-            var item1 = new Product(
-                Console.ReadLine(),
-                decimal.Parse(Console.ReadLine()),
-                int.Parse(Console.ReadLine()),
-                Console.ReadLine()
-                );
+            var item1 = new Product("Chocolate", 0.99m, 5);
 
+            Console.WriteLine("The product {0} which is {1} costs {2:C} there are {3} left in stock", item1.Name, item1.Description, item1.Price, item1.Stock);
+            Console.WriteLine("Would you like to add a description?");
+            if (Console.ReadLine() == "yes")
+            {
+                Console.WriteLine("Enter description");
+                item1.Description = Console.ReadLine();
+                Console.WriteLine("The product {0} which is {1} costs {2:C} there are {3} left in stock", item1.Name, item1.Description, item1.Price, item1.Stock);
+            }
+            else
+            {
+                Console.WriteLine("Thank you");
+            }
+            Console.ReadLine();
         }
     }
 }
