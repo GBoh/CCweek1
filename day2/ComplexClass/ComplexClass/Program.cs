77f7b90b98813a4ee0a4c4299305f440bb7ff75b
GBoh <greg@bohanon3d.com>
2015-08-18 21:46:55 -0500

    Complex banking class, no ExC

---
+++ day2/ComplexClass/ComplexClass/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexClass
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsOpen { get; set; }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            this.Checking = new Account();
+            this.Savings = new Account();
+        }
+
+        public string Name { get; set; }
+
+        //Checking Account
+        public Account Checking { get; set; }
+
+        //Savings Account
+        public Account Savings { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer
+            {
+                //sets name
+                Name = "Alice",
+
+                //sets checking account
+                Checking =
+                {
+                    Id=1234,
+                    Amount=100.00m,
+                    Type="Checking",
+                    IsOpen = true
+                },
+
+                //sets Savings account
+                Savings =
+                {
+                    Id=4321,
+                    Amount=0m,
+                    Type="Savings",
+                    IsOpen = false
+                }
+
+            };
+
+            Console.WriteLine(customer.Name + " has {0:C} in account {1} ", customer.Checking.Amount, customer.Checking.Type);
+            Console.ReadLine();
+        }
+    }
+}
