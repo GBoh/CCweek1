60dbcd700c605cd83f51a560056cae8d790c2d3a
GBoh <greg@bohanon3d.com>
2015-08-19 14:46:52 -0500

    BetterTryParse added

--- day3/BetterTryParse/BetterTryParse/Properties/AssemblyInfo.cs
+++ day3/BetterTryParse/BetterTryParse/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AnimalInterface")]
+[assembly: AssemblyTitle("BetterTryParse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AnimalInterface")]
+[assembly: AssemblyProduct("BetterTryParse")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d959ed8-1293-439a-a11c-4da9def7f55d")]
+[assembly: Guid("ef25bab1-4fb9-4e6c-a6c2-ac9132390d52")]
 
 // Version information for an assembly consists of the following four values:
 //
