74d6d102800cf366823046c6f5d4c12fbf8acc6d
GBoh <greg@bohanon3d.com>
2015-08-19 10:23:25 -0500

    can now chose which property to edit

--- day2/ProductWithConstructor/ProductWithConstructor/Program.cs
+++ day2/ProductWithConstructor/ProductWithConstructor/Program.cs
@@ -13,18 +13,36 @@ namespace ProductWithConstructor
         {
             var item1 = new Product("Chocolate", 0.99m, 5);
 
-            Console.WriteLine("The product {0} which is {1} costs {2:C} there are {3} left in stock", item1.Name, item1.Description, item1.Price, item1.Stock);
-            Console.WriteLine("Would you like to add a description?");
-            if (Console.ReadLine() == "yes")
+            Console.WriteLine("The product {0} costs {2:C}, there are {3} left in stock. This product is {1}", item1.Name, item1.Description, item1.Price, item1.Stock);
+
+            Console.WriteLine("Which property would you like to edit?");
+            var type = Console.ReadLine();
+            switch (type)
             {
-                Console.WriteLine("Enter description");
-                item1.Description = Console.ReadLine();
-                Console.WriteLine("The product {0} which is {1} costs {2:C} there are {3} left in stock", item1.Name, item1.Description, item1.Price, item1.Stock);
+                case "name":
+                    Console.WriteLine("Enter new " + type);
+                    item1.Name = Console.ReadLine();
+                    break;
+
+                case "price":
+                    Console.WriteLine("Enter new " + type);
+                    item1.Price = decimal.Parse(Console.ReadLine());
+                    break;
+
+                case "stock":
+                    Console.WriteLine("Enter new " + type);
+                    item1.Stock = int.Parse(Console.ReadLine());
+                    break;
+
+                case "description":
+                    Console.WriteLine("Enter new " + type);
+                    item1.Description = Console.ReadLine();
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Thank you");
-            }
+
+            Console.WriteLine("The product {0} costs {2:C}, there are {3} left in stock. This product is {1}", item1.Name, item1.Description, item1.Price, item1.Stock);
             Console.ReadLine();
         }
     }
