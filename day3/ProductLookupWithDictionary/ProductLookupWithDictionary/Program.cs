6de80bdebf30b74a67d52a4b19a1b85b1161bd00
GBoh <greg@bohanon3d.com>
2015-08-20 12:29:13 -0500

    ProductLookupWithDictionary finished

--- day3/ProductLookupWithDictionary/ProductLookupWithDictionary/Program.cs
+++ day3/ProductLookupWithDictionary/ProductLookupWithDictionary/Program.cs
@@ -15,17 +15,35 @@ namespace ProductLookupWithDictionary
 
     public class ProductCatalog
     {
-        Dictionary<int, string> catalog = new Dictionary<int, string>();
+        Dictionary<int, Product> catalog = new Dictionary<int, Product>();
         public string LookupId(int id)
         {
-
-
-            return null;
+            return catalog[id].Name;
         }
 
         public ProductCatalog()
         {
-            
+            catalog.Add(9875, new Product
+            {
+                Id = 9875,
+                Name = "Salad",
+                Price = 15.00m
+            });
+
+            catalog.Add(1234, new Product
+            {
+                Id = 1234,
+                Name = "Water",
+                Price = 5.00m
+            });
+
+
+            catalog.Add(4321, new Product
+            {
+                Id = 4321,
+                Name = "Soda",
+                Price = 0.99m
+            });
         }
     }
 
@@ -33,6 +51,9 @@ namespace ProductLookupWithDictionary
     {
         static void Main(string[] args)
         {
+            var catalog = new ProductCatalog();
+            Console.WriteLine(catalog.LookupId(1234));
+            Console.ReadLine();
         }
     }
 }
