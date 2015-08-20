89bd80411498f4543a7a3bc6c2ddf516c81efd66
GBoh <greg@bohanon3d.com>
2015-08-20 10:26:56 -0500

    creating anothe ProductCatalog but using dictionary this time

---
+++ day3/ProductLookupWithDictionary/ProductLookupWithDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLookupWithDictionary
+{
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+    }
+
+    public class ProductCatalog
+    {
+        Dictionary<int, string> catalog = new Dictionary<int, string>();
+        public string LookupId(int id)
+        {
+
+
+            return null;
+        }
+
+        public ProductCatalog()
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
