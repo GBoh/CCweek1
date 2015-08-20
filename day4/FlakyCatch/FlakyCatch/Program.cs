a51caf5003f611818b0b9310ead4648633c2441d
GBoh <greg@bohanon3d.com>
2015-08-20 14:48:46 -0500

    Flaky Program finished

---
+++ day4/FlakyCatch/FlakyCatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlakyCatch
+{
+    class Program
+    {
+
+        public static void Flaky()
+        {
+            if (new Random().NextDouble() < 0.5)
+            {
+                Console.WriteLine("Finished calling Flaky");
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Flaky();
+            }
+            catch
+            {
+                Console.WriteLine("Yikes! There was an error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
