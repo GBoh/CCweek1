23e28a0c3a394142d3b17973540e9e2101376772
GBoh <greg@bohanon3d.com>
2015-08-20 16:14:25 -0500

    OrgChart init

---
+++ day3/OrgChart/OrgChart/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgChart
+{
+    interface IEmployee
+    {
+        int Underlings { get; set; }
+    }
+
+    class Bill : IEmployee
+    {
+        public int Underlings { get; set; }
+    }
+
+    class Sam : IEmployee
+    {
+        public int Underlings { get; set; }
+    }
+
+    class Fred : IEmployee
+    {
+        public int Underlings { get; set; }
+    }
+
+    class Jane : IEmployee
+    {
+        public int Underlings { get; set; }
+    }
+
+    class Alice : IEmployee
+    {
+        public int Underlings { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CEO = new Bill();
+        }
+    }
+}
