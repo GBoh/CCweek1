eb0b353e648b21e74d065ff5011d16d8b4f680e5
GBoh <greg@bohanon3d.com>
2015-08-20 13:18:54 -0500

    finished ProfanityCleaner

--- day3/ProfanityCleaner/ProfanityCleaner/Program.cs
+++ day3/ProfanityCleaner/ProfanityCleaner/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace ProfanityCleaner
 {
+    class ProfanityCleaner
+    {
+        Dictionary<string, string> badWords = new Dictionary<string, string>();
+        public ProfanityCleaner()
+        {
+            badWords.Add("darn", "drat");
+            badWords.Add("gosh", "golly gee willikers");
+            badWords.Add("yuck", "yech");
+        }
+
+        internal string Clean(string sentence)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var wordsArr = sentence.Split(' ');
+            foreach (var word in wordsArr)
+            {
+                if (badWords.ContainsKey(word))
+                {
+                    sb.Append(badWords[word] + " ");
+                }
+                else
+                {
+                    sb.Append(word + " ");
+                }
+
+            }
+            //Console.WriteLine(sb.ToString());
+            return sb.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var chatMonitor = new ProfanityCleaner();
+            Console.WriteLine("gosh darn it I hate those green things. yuck \n\n");
+
+            Console.WriteLine(chatMonitor.Clean("gosh darn it I hate those green things. yuck"));
+            Console.ReadLine();
         }
     }
 }
