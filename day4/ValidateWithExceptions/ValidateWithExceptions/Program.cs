e7d7d4a1a0c9c12f3338ff4ba98d1e760f46a537
GBoh <greg@bohanon3d.com>
2015-08-20 13:54:29 -0500

    exemption test finished

--- day4/ValidateWithExceptions/ValidateWithExceptions/Program.cs
+++ day4/ValidateWithExceptions/ValidateWithExceptions/Program.cs
@@ -8,8 +8,25 @@ namespace ValidateWithExceptions
 {
     class Program
     {
+        public static decimal CalculateTotalPriceWithTax(decimal price, decimal taxRate)
+        {
+            if (taxRate > 10 || taxRate <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Tax Rate", "Your taxrate of " + taxRate + " is either above 10% or less than 0%");
+            }
+
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Price", "Your product price cannot be $0.00 or less");
+            }
+
+            return Math.Round(price + (price * (taxRate / 100)), 2);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(CalculateTotalPriceWithTax(1.89m, 5m));
+            Console.ReadLine();
         }
     }
 }
