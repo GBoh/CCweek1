fc0825359951c4159f61af8c9d28d1e0181e2b91
GBoh <greg@bohanon3d.com>
2015-08-19 22:55:39 -0500

    added find unique words

---
+++ day3/CountUniqueWords/CountUniqueWords/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountUniqueWords
+{
+    class Program
+    {
+        static int CountWords(string sentence)
+        {
+            Hashtable uniqueWords = new Hashtable();
+            var lower = sentence.ToLower();
+            var wordArr = lower.Split(' ');
+            int key = 0;
+
+            foreach (var word in wordArr)
+            {
+                if (!uniqueWords.ContainsKey(word))
+                {
+                    uniqueWords.Add(word, key+=1);
+                }
+            }
+            return key;
+        }
+
+        static void Main(string[] args)
+        {
+            ;
+            Debug.Assert(CountWords("The cat and the cat ate the rat") == 5);
+        }
+    }
+}
