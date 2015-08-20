dc425a406b525dc82a914a1e32517cc38ba41c66
GBoh <greg@bohanon3d.com>
2015-08-20 14:48:23 -0500

    init ValidCustomerClass

---
+++ day4/ValidCustomerClass/ValidCustomerClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidCustomerClass
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+
+    class Program
+    {
+        public static void testValidation(string firstName, string lastName)
+        {
+            if (firstName == null || lastName == null)
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var customer1 = new Customer();
+            customer1.FirstName = "Bill";
+            customer1.LastName = "Gates";
+            //Console.WriteLine(customer1.FirstName);
+            testValidation(customer1.FirstName, customer1.LastName);
+            //Console.ReadLine();
+        }
+    }
+}
