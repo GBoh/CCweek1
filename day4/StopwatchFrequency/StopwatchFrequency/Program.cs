ee82fe906b6cd1b1e821598eb3fc41ae7e2ca5c6
GBoh <greg@bohanon3d.com>
2015-08-20 21:02:51 -0500

    StopwatchFrequency from codercamp site

---
+++ day4/StopwatchFrequency/StopwatchFrequency/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Supports High Resolution StopWatch? {0}", Stopwatch.IsHighResolution);
+            Console.WriteLine("Stopwatch Frequency? {0}", Stopwatch.Frequency);
+
+            // Pause
+            Console.ReadLine();
+        }
+    }
+}
